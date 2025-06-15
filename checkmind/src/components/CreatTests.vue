21ee627ee91db104e8ef0c3f8d0321d7b4283323
Egor Staroverov <staroverov.egor.2010@gmail.com>
2025-06-15 17:24:19 +0300

    рабочая валидация форм помимо сравнения чисел

--- checkmind/src/components/CreatTests.vue
+++ checkmind/src/components/CreatTests.vue
@@ -15,48 +15,46 @@
                     <div class="card">
                         <div class="card-body d-flex flex-column">
                             <h5 class="card-title">Создание нового теста</h5>
-                            <form>
-                                <input 
-                                @blur="v$.test_name.$touch"
-                                class="form-control"
-                                type="text"
-                                placeholder="Название теста"
-                                v-model="test_name">
-                                <p class="error-alert" v-if="v$.test_name.$error">Поле не должно быть пустым</p>
-                                <hr>
-                                <input
-                                @blur="v$.group_id.$touch"
-                                class="form-control"
-                                type="text"
-                                placeholder="ID-группы"
-                                v-model="group_id">
-                                <p class="error-alert" v-if="v$.group_id.$error">Поле не может быть пустым и должно содержать только цифры</p>
-                                <hr>
-                                <input
-                                @blur="v$.questions_quantity.$touch"
-                                class="form-control"
-                                type="number"
-                                placeholder="Количество вопросов"
-                                v-model="questions_quantity">
-                                <p class="error-alert" v-if="v$.questions_quantity.$error">Поле не может быть пустым и должно содержать только цифры</p>
-                                <hr>
-                                <input
-                                @blur="v$.answers_quantity.$touch"
-                                class="form-control"
-                                type="number"
-                                placeholder="Количество ответов"
-                                v-model="answers_quantity">
-                                <p class="error-alert" v-if="v$.answers_quantity.$error">Поле не может быть пустым и должно содержать только цифры</p>
-                                <hr>
-                                <input
-                                @blur="v$.correct_answers_quantity.$touch"
-                                class="form-control"
-                                type="number"
-                                placeholder="Количество верных ответов"
-                                v-model="correct_answers_quantity">
-                                <p class="error-alert" v-if="v$.correct_answers_quantity.$error">Поле не может быть пустым и должно содержать только цифры</p>
-                                <button name="create_test" class="mt-auto btn btn-primary w-100 disabled">Перейти к написанию заданий</button>
-                            </form>
+                            <input
+                            v-model="v$.test_name.$model"
+                            class="form-control"
+                            type="text"
+                            placeholder="Название теста"
+                            @blur="v$.test_name.$touch()">
+                            <p class="error-alert" v-if="v$.test_name.$error">Поле не должно быть пустым</p>
+                            <hr>
+                            <input
+                            v-model="v$.group_id.$model"
+                            @blur="v$.group_id.$touch"
+                            class="form-control"
+                            type="text"
+                            placeholder="ID-группы">
+                            <p class="error-alert" v-if="v$.group_id.$error">Поле не может быть пустым и должно содержать только цифры</p>
+                            <hr>
+                            <input
+                            v-model="v$.questions_quantity.$model"
+                            @blur="v$.questions_quantity.$touch"
+                            class="form-control"
+                            type="number"
+                            placeholder="Количество вопросов">
+                            <p class="error-alert" v-if="v$.questions_quantity.$error">Поле не может быть пустым и должно содержать только цифры</p>
+                            <hr>
+                            <input
+                            v-model="v$.answers_quantity.$model"
+                            @blur="v$.answers_quantity.$touch"
+                            class="form-control"
+                            type="number"
+                            placeholder="Количество ответов">
+                            <p class="error-alert" v-if="v$.answers_quantity.$error">Поле не может быть пустым и должно содержать только цифры</p>
+                            <hr>
+                            <input
+                            v-model="v$.correct_answers_quantity.$model"
+                            @blur="v$.correct_answers_quantity.$touch"
+                            class="form-control"
+                            type="number"
+                            placeholder="Количество верных ответов">
+                            <p class="error-alert" v-if="v$.correct_answers_quantity.$error">Поле не может быть пустым и должно содержать только цифры</p>
+                            <button name="create_test" class="mt-auto btn btn-primary w-100 disabled">Перейти к написанию заданий</button>
                         </div>
                     </div>
                 </div>
@@ -69,8 +67,7 @@
 <script>
 import useVuelidate from '@vuelidate/core';
 import { numeric, required } from '@vuelidate/validators';
-import { reactive } from 'vue';
-import { ref } from 'vue'
+import { ref, computed, reactive } from 'vue';
 
 export default {
     setup() {
@@ -82,13 +79,13 @@ export default {
             answers_quantity: '',
             correct_answers_quantity: '',
         })
-        const rules = {
+        const rules = computed(() => ({
             test_name: {required},
             group_id: {required, numeric},
             questions_quantity: {required, numeric},
             answers_quantity: {required, numeric},
             correct_answers_quantity: {required, numeric},
-        }
+        }))
         const v$ = useVuelidate(rules, state)
 
         return {state, v$, user}
