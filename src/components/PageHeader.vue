33f927fce80e6291b815a551b1a6648b0a6fdbcf
Egor Staroverov <staroverov.egor.2010@gmail.com>
2025-07-27 12:09:52 +0300

    немного исправлений динамической отрисовки

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,10 +1,7 @@
 <template>
 <nav class="navbar navbar-expand-md sticky-top navbar-dark bg-dark">
   <div class="container-fluid">
-    <h2><a class="navbar-brand" href="/">CheckMind</a></h2>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+    <h2><router-link class="navbar-brand" to="/">CheckMind</router-link></h2>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
         <li class="nav-item">
