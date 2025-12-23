2b12873b17bdebb61cd1dbe187cae45c9b65112f
Egor Staroverov <staroverov.egor.2010@gmail.com>
2025-12-23 19:40:58 +0300

    Подключил библиотеку ElementPlus для использования ElTable и остального для таблиц

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { createApp } from 'vue'
 import router from './router/routes.js'
 import store from './store'
 import i18n from './locales/i18n';
-
+import ElementPlus from 'element-plus';
 import {createBootstrap} from 'bootstrap-vue-next'
 import { useVuelidate } from '@vuelidate/core';
 
@@ -12,9 +12,11 @@ import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'element-plus/dist/index.css';
 
 const app = createApp(App)
 app.use(createBootstrap())
+app.use(ElementPlus)
 app.use(useVuelidate);
 app.use(i18n);
 app.use(router)
